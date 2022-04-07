b654bf102cff2b42e13c20388fdd6d009f1b2ee1
jalonsouoc <jalonsohedz@uoc.edu>
2022-04-07 19:38:21 +0100

    Probando a obtener el tiempo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 def download_html(url):
     http = urllib3.PoolManager()
     response = http.request('GET', url)
-    soup = BeautifulSoup(response.data.decode('utf-8'), "lxml")
+    soup = BeautifulSoup(response.data, "lxml")
     return soup
 
 def get_links(items):
@@ -19,6 +19,7 @@ def get_links(items):
     return links
 
 def get_book_information(url, links):
+    books = []
     for link in links:
         print(url + link)
         sp = download_html(url + link)
@@ -27,8 +28,12 @@ def get_book_information(url, links):
         for book in books_info:
             name = book.find_all("h1", class_="text-h4 mb-2")
             print(name[0].text.strip())
+            books.append({name})
 
-url = "https://www.casadellibro.com/libro-y-pelicula"
+
+
+
+url = "http://www.aemet.es/es/eltiempo/prediccion/municipios?p=38&w=t"
 data = []
 dataset_file = "dataset.csv"
 
@@ -42,19 +47,45 @@ print("Iniciamos la descarga de la web")
 soup = download_html(url)
 
 
-items = soup.find_all("div", class_="d-flex flex-column align-center pa-2 item")
+items = soup.find_all("td")
+
 
 print("Items encontrados: " + str(len(items)))
+dataMunicipios = []
+for item in items:
+    municipio = item.find_all("a", href=True)
+    urlMunicipio = municipio[0]['href'].split('/')
+    dataMunicipios.append([municipio[0].text.strip(), urlMunicipio[len(urlMunicipio) - 1]])
+ 
+
+def get_prediccion_municipio(url):
+    bs_municipio = download_html(url)
+    #Obtenemos la fecha
+    fecha = bs_municipio.find_all("th", class_="borde_izq_dcha_fecha")[0].text.strip()
+    print(fecha)
+
+    #Obtenemos las horas
+    hora = bs_municipio.find_all("div", class_="fuente09em")[0].text.strip()
+    print(hora)
+
+for municipio in dataMunicipios:
+    print(municipio[0])
+    url_municipio = "http://www.aemet.es/es/eltiempo/prediccion/municipios/" + municipio[1]
+    get_prediccion_municipio(url_municipio)
+
+
+
+
 
 
     #print(sp)
     
-links = get_links(items)
+#links = get_links(items)
 
 
 #for l in links:
     #sp = download_html("https://www.casadellibro.com/" + l)
-get_book_information("https://www.casadellibro.com/", links)
+#get_book_information("https://www.casadellibro.com/", links)
 
 
 
